d03086f8c47f16fec2d22f69f3d75f54e71187fc
d-klotz <danielfelipeklotz@gmail.com>
2026-01-22 12:43:02 -0300

    chore(scripts): update SQS purge script with correct queues

--- scripts/purge-local-sqs-queues.sh
+++ scripts/purge-local-sqs-queues.sh
@@ -3,13 +3,14 @@
 # Purge all local SQS queues in LocalStack
 echo "Purging local SQS queues..."
 
-aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/create-frigg-app--dev-AsanaQueue" && \
-aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/create-frigg-app--dev-CanvaQueue" && \
-aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/quo-integrations--dev-ScalingtestQueue" && \
-aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/quo-integrations--dev-PipedriveQueue" && \
-aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/quo-integrations--dev-AxisCareQueue" && \
-aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/quo-integrations--dev-AttioQueue" && \
 aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/quo-integrations--dev-ZohoQueue" && \
-aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/quo-integrations--dev-ClioQueue"
+aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/quo-integrations--dev-ScalingtestQueue" && \
+aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/quo-integrations--dev-AttioQueue" && \
+aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/quo-integrations--dev-AxisCareQueue" && \
+aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/quo-integrations--dev-PipedriveQueue" && \
+aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/quo-integrations--dev-ClioQueue" && \
+aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/freshbooks-frigg--dev-HubspotSyncContactQueueWorker" && \
+aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/freshbooks-frigg--dev-GenericQueueWorker" && \
+aws --endpoint-url=http://localhost:4566 sqs purge-queue --queue-url "http://localhost.localstack.cloud:4566/000000000000/freshbooks-frigg--dev-SquareTokenRefresherQueue"
 
 echo "✓ All queues purged successfully"
